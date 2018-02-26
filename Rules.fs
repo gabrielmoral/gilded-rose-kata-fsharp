b3b758f269c50f5897bfe1e050aee72e3c4cd845
Gabriel Moral <gabrielmoralortiz@gmail.com>
2018-02-26 19:34:03 +0000

    Fix errors and compose functions instead of group the behavior in one place

--- Rules.fs
+++ Rules.fs
@@ -19,35 +19,42 @@ let increaseQualityBy quality item =
 
 let decreaseQualityBy quality item =
     let minQuality = 0
-    let exceededMinQuality = item.Quality - minQuality <= minQuality
+    let exceededMinQuality = item.Quality - quality <= minQuality
     if exceededMinQuality then { item with Quality = minQuality }
     else { item with Quality = item.Quality - quality }
 
 let noQuality item = { item with Quality = 0 }
 
-let (|After|_|) x = if x = 0 then Some After else None
-let (|Before|_|) x = if x <= 5 then Some Before else None  
-let (|LongBefore|_|) x = if x <= 10 then Some LongBefore else None  
+let (|AfterSellIn|_|) x = if x = 0 then Some AfterSellIn else None
+let (|BeforeSellIn|_|) x = if x <= 5 then Some BeforeSellIn else None  
+let (|LongBeforeSellIn|_|) x = if x <= 10 then Some LongBeforeSellIn else None
 
-let calculateQuality item = 
-    match item.Type with
-    | Some AgedBrie -> increaseQualityBy 1
-    | Some BackStagePass -> 
-        match item.SellIn with
-        | After -> noQuality
-        | Before -> increaseQualityBy 3
-        | LongBefore -> increaseQualityBy 2
-        | _ -> increaseQualityBy 1
-    | None ->
-        match item.SellIn with
-        | 0 -> decreaseQualityBy 2
-        | _ -> decreaseQualityBy 1
+let agedBrieQuality item =
+    item |> increaseQualityBy 1
 
-let decreaseDate item = { item with SellIn = item.SellIn - 1 }
+let backStagePassQuality item =
+    let calculator = match item.SellIn with
+                     | AfterSellIn -> noQuality
+                     | BeforeSellIn -> increaseQualityBy 3
+                     | LongBeforeSellIn -> increaseQualityBy 2
+                     | _ -> increaseQualityBy 1
+    item |> calculator
+
+let calculateItemQuality item =
+    let calculator = match item.SellIn with
+                     | AfterSellIn -> decreaseQualityBy 2
+                     | _ -> decreaseQualityBy 1
+    item |> calculator
+
+let decreaseDate item = 
+    if item.SellIn = 0 then item
+    else { item with SellIn = item.SellIn - 1 }
 
 let processItem item =
     let processor = match item.Type with
                     | Some Sulfuras -> id
-                    | _ -> decreaseDate >> calculateQuality item
+                    | Some AgedBrie -> decreaseDate >> agedBrieQuality
+                    | Some BackStagePass -> decreaseDate >> backStagePassQuality
+                    | None -> decreaseDate >> calculateItemQuality
     
     item |> processor
